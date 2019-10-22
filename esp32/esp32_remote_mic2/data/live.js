255445e15c6db9b2e597317bb26483672e09f939
lixy123 <35212560+lixy123@users.noreply.github.com>
2019-10-22 22:45:17 +0800

    Add files via upload

--- esp32/esp32_remote_mic2/data/live.js
+++ esp32/esp32_remote_mic2/data/live.js
@@ -8,17 +8,18 @@ let config = {
  
  
 $("#listen").on('click', function() {
-	//alert("listen click")
-	console.log("click on listen");
 	if (player.isPlaying()) {
-		$(this).text("silence");		
+        console.log("trigger listen off");
+		$(this).text("listen off");		
 		player.stop();
 	}
 	else {
-		$(this).text("liston")
+        console.log("trigger listen on");
+		$(this).text("listen on")
 		player.play();
-		//setTimeout(function(){ doSend("hello"); }, 5000);
-		doSend("10"); 
+        //延时，确保连接成功后再发送信息
+		setTimeout(function(){ doSend("0"); },1000);
+		//doSend("600"); 
 	}
 });
 
@@ -57,7 +58,8 @@ function onClose(evt) {
 // Called when a message is received from the server
 function onMessage(evt) { 
     // Print out our received message
-    console.log("Received: " + evt.data);
+    //blob对象
+    //console.log(evt.data);
 		
 	player.write(evt.data);   
 }
@@ -69,6 +71,12 @@ function onError(evt) {
  
 // Sends a message to the server (and prints it to the console)
 function doSend(message) {
+    //ESP32是单线程，同一函数一次只能同时服务一个
+    //一次只能同时服务一个
+    //执行前最好检查下当前连接状态
+    //否则容易出现如下错误
+    //Uncaught DOMException: Failed to execute 'send' on 'WebSocket': 
+    //Still in CONNECTING state.
     console.log("Sending: " + message);
     websocket.send(message);
 }
--- esp32/esp32_remote_mic2/data/player.js
+++ esp32/esp32_remote_mic2/data/player.js
@@ -4,17 +4,20 @@ class Player {
 		this.config = config;
 		this.socket = socket;
 		this.audioCtx = null;
-		this.silence = new  Int8Array(this.config.bufferSize);
+		this.silence = new  Float32Array(this.config.bufferSize);
 		this.playing = false;
 
+        
 		this.audioQueue = {
-			buffer: new Int8Array(0),
+			buffer: new Float32Array(0),
 
 			write: function(chunk) {
 				let currentLength = this.buffer.length;
-				let newBuffer = new Int8Array(currentLength + chunk.length);
+				let newBuffer = new Float32Array(currentLength + chunk.length);
+                //console.log("currentLength",currentLength,newBuffer.length,chunk.length)
 				newBuffer.set(this.buffer, 0);
 				newBuffer.set(chunk, currentLength);
+                //console.log("newBuffer",newBuffer.length)
 				this.buffer = newBuffer;
 			},
 
@@ -29,7 +32,7 @@ class Player {
 			},
 
 			clear: function() {
-				this.buffer = new Int8Array(0)
+				this.buffer = new Float32Array(0)
 			}
 		}
 
@@ -38,9 +41,19 @@ class Player {
 	play() {
 		this.playing = true;
 		this.audioCtx = new AudioContext();
+        
+        //48000频率 只读属性
+        //16000频率的声音传入会导致 3倍加速
+        console.log(this.audioCtx.sampleRate);
+        //this.audioCtx.sampleRate= 16000;
+        
 		this.scriptNode = this.audioCtx.createScriptProcessor(this.config.bufferSize, 1, 1);
+        console.log("play") 
 		this.scriptNode.onaudioprocess = (e) => {
+               // console.log("audioQueue") 
+               // console.log(this.audioQueue.length())             
 			if (this.audioQueue.length()) {
+
 				e.outputBuffer.getChannelData(0).set(this.audioQueue.read(this.config.bufferSize));
 			}
 			else {
@@ -51,15 +64,57 @@ class Player {
 		this.scriptNode.connect(this.audioCtx.destination);
 	}
 
+    //将传入的16000频率的声音数据转成48000频率的声音数据
+    interleave(e){
+      let t = e.length;
+      let sampleRate = 16000.0 ;
+      let outputSampleRate = 48000.0;
+      let s = 0;
+      let o = sampleRate / outputSampleRate;
+      let u = Math.ceil(t * outputSampleRate / sampleRate);      
+      let a = new Float32Array(u);
+      let i=0;
+      for (i = 0; i < u; i++) {
+        a[i] = e[Math.floor(s)];
+        s += o;
+      }
+      return a;
+    }
+    
+    //MDN上说，audioporcess缓冲区的数据是，非交错的32位线性PCM，标称范围介于-1和之间+1，
+    //即32位浮点缓冲区，每个样本介于-1.0和1.0之间。
+    //http://www.i2geek.com/mobile/articleContent.php?id=19544
+    //https://webaudio.github.io/web-audio-api/#APIOverview
     write(chunk) {
-		//let array = new Float32Array(chunk);
-		//this.audioQueue.write(array);	
-        console.log(chunk)   
-        let array =new Int8Array(chunk);
-        console.log(chunk.length) 
-        this.audioQueue.write(array);
-	
+		//将传入的blob转换成 Float32Array对象 
+        var reader = new FileReader(); 
+        var my_audioQueue= this.audioQueue;
+        var my_int16ToFloat32= this.int16ToFloat32
+        var my_interleave= this.interleave;
+        reader.onload = function(e) {
+           //console.log(reader.result); 
+           let new_chunk=reader.result;          
+  
+           //A.如果输入是: 16000hz 16位声音数据            
+           //let testDataInt = new Int8Array(new_chunk);
+           let testDataInt = new Int16Array(new_chunk); 
+           //16位对象转32位对象                
+           let array = my_int16ToFloat32(testDataInt,0,testDataInt.length);
+           //16000hz 转 48000hz
+           array= my_interleave(array);           
+           //console.log(array.length,array);  
+           console.log("get wav bytes:",array.length);
+           //B.如果输入是: 48000hz 16位声音数据
+           /*
+           let array = new Float32Array(new_chunk);
+           console.log(array.length,array);              
+		   */           
+		   my_audioQueue.write(array);       
+        }; 
+        reader.readAsArrayBuffer(chunk);        
 	}
+    
+    
 	
 	stop() {
 		this.audioQueue.clear();
@@ -71,5 +126,14 @@ class Player {
 		return !! this.scriptNode;
 	}
 
-
+    int16ToFloat32(inputArray, startIndex, length) {
+		var output = new Float32Array(inputArray.length-startIndex);
+		for (var i = startIndex; i < length; i++) {
+			var int = inputArray[i];
+			// If the high bit is on, then it is a negative number, and actually counts backwards.
+			var float = (int >= 0x8000) ? -(0x10000 - int) / 0x8000 : int / 0x7FFF;
+			output[i] = float;
+		}
+		return output;
+	}
 }
